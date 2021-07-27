6fa3026d4103bf485563d54b56674757d3106934
Toby Law <toby@tzcl.me>
2021-07-27 21:49:57 +1000

    Refactored tracker

--- tracker.py
+++ tracker.py
@@ -1,57 +1,68 @@
 #!/usr/bin/env python3
 
-from skyfield.api import load, wgs84
-from datetime import datetime, timezone, timedelta
+"""
+This is a script that tracks the trajectory of a satellite over time.
 
+Input: satellite ID (either NORAD ID or international code).
+Output: satellite trajectory in terms of long/lat over time.
+"""
+
+from argparse import ArgumentParser
+from time import sleep
+from skyfield.api import load, wgs84
+
+# Read satellite id from command line
+parser = ArgumentParser()
+parser.add_argument("id", help="satellite id to track")
+parser.add_argument('--reload',
+                    help="force redownload of satellite TLE",
+                    action="store_true")
+args = parser.parse_args()
+
+sat_id = args.id
+reload_tle = args.reload
+
+# Parse satellite id
+# International ids are of the form YYYY-NNNA, e.g., 2017-073A
+# International ids may be the same for multiple satellites
+# NORAD ids are 5 digit numbers
+is_norad = not sat_id[-1].isalpha()
+sat_offs = 0
+if not is_norad:
+    # Use the letter on the end of id to determine satellite
+    sat_offs = ord(sat_id[-1]) - ord('A')
+    sat_id = sat_id[:-1]
+
+# Load TLE and parse into EarthSatellite
+request_type = 'CATNR' if is_norad else 'INTDES'
+url = f'https://celestrak.com/satcat/tle.php?{request_type}={sat_id}'
+filename = f'{request_type}-{sat_id}.txt'
+satellites = load.tle_file(url, filename=filename, reload=reload_tle)
+if not satellites:
+    raise Exception("Failed to load satellites")
+sat = satellites[sat_offs]
+
+# Check epoch is valid
 ts = load.timescale()
 
-# Load TLE
-url = 'https://celestrak.com/satcat/tle.php?INTDES=2009-005'
-filename = 'NOAA-19.txt'
-satellites = load.tle_file(url, filename=filename)
-satellite = satellites[0]
-
-# Calculate when it passes overhead
-melbourne = wgs84.latlon(-37.814, 144.96332)
-t0 = ts.utc(2021, 7, 23)
-t1 = ts.utc(2021, 7, 24)
-t, events = satellite.find_events(melbourne, t0, t1, altitude_degrees=0)
-for ti, event in zip(t, events):
-    name = ('above horizon', 'culminate', 'below horizon')[event]
-    print(ti.utc_strftime('%Y %b %d %H:%M:%S'), name)
-
-
-# Current position
 t = ts.now()
-geocentric = satellite.at(t)
+days = t - sat.epoch
+if abs(days) > 14:
+    print(f"WARNING: TLE may be out of date ({days} days away from epoch). "
+          "Try running with '--reload'.")
 
-subpoint = wgs84.subpoint(geocentric)
-print('Latitude:', subpoint.latitude)
-print('Longitude:', subpoint.longitude)
-print('Height: {:.1f} km'.format(subpoint.elevation.km))
+# Calculate distance
+melbourne = wgs84.latlon(-37.814, 144.96332)  # TODO: hardcoded!
+diff = sat - melbourne
 
-# Relative position
-t = ts.now()
-difference = satellite - melbourne
-topocentric = difference.at(t)
+while True:
+    t = ts.now()
+    topocentric = diff.at(t)
 
-alt, az, distance = topocentric.altaz()
-print('Altitude:', alt)
-print('Azimuth:', az)
-print('Distance: {:.1f} km'.format(distance.km))
+    alt, az, distance = topocentric.altaz()
+    print("Time:", t.utc_strftime("%Y-%m-%d %H:%M:%S"))
+    print('Altitude:', alt)
+    print('Azimuth:', az)
+    print('Distance: {:.1f} km'.format(distance.km))
 
-# Check if the satellite passes overhead
-# Need to correct for timezone!
-# AEST is 10 hours ahead, so need to subtract 10 to get UTC
-one_day = ts.utc(2021, 7, 23, 20 - 10, range(0, 60))
-topocentric = difference.at(one_day)
-
-alt, az, distance = topocentric.altaz()
-for count, time in enumerate(one_day):
-    print("Time: ", time.utc_strftime("%Y-%m-%d %H:%M:%S"))
-    print("Altitude:", alt.degrees[count])
-    print("Azimuth:", az.degrees[count])
-    print("Distance: {:.1f} km".format(distance.km[count]))
-
-print("------------------------------------------------")
-print(min(distance.km))
+    sleep(1)
